839c846e0548cf481e78c2ca1cac78cde1af18d6
17612115819 <amos@houjit.com>
2022-09-16 19:54:52 +0800

    amos

--- src/DateInfo.php
+++ src/DateInfo.php
@@ -1,7 +1,17 @@
 <?php
-namespace SinMing\ChinaHoliday;
-
-use SinMing\ChinaHoliday\ChinaHolidayHelper;
+// +----------------------------------------------------------------------
+// | HouCMF [ 用心做好每个站 用心服务好每个客户 ]
+// +----------------------------------------------------------------------
+// | Copyright (c) 2006-2019 http://www.houjit.com All rights reserved.
+// +----------------------------------------------------------------------
+// | Licensed ( http://www.apache.org/licenses/LICENSE-2.0 )
+// +----------------------------------------------------------------------
+// | Author: Amos <amos@houjit.com>
+// +----------------------------------------------------------------------
+// |
+// +----------------------------------------------------------------------
+namespace houjit\houholiday;
+use houjit\houholiday\Helper;
 
 class DateInfo
 {
